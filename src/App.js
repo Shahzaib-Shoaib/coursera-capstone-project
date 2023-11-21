a305607de81f69d9caeb36fead78b45e32178e86
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-11-21 20:20:30 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import "./App.css";
-import Header from "./components/header";
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
+
 function App() {
   return (
     <>
-      <div className="">
-        <Header />
-        <main></main>
-        <footer></footer>
-      </div>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
     </>
   );
 }
